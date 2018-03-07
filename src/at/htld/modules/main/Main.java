f9a3acec2016b5331735f250c7989f796f0c5389
jusic <jusic.husein@hotmail.com>
2018-03-07 15:49:51 +0100

    add db Handler

--- src/at/htld/modules/main/Main.java
+++ src/at/htld/modules/main/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
         primaryStage.setScene(loginScene);
         primaryStage.show();
     }
-
+    
 
     public static void main(String[] args) {
         launch(args);
