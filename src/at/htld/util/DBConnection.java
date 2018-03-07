259dc95bb9e981bcb64418884f26b23cd476a96d
Michael Brida <michael.brida@cable.vol.at>
2018-03-07 15:56:48 +0100

    create dbconnetion

---
+++ src/at/htld/util/DBConnection.java
@@ -0,0 +1,32 @@
+package at.htld.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private final String DB_DATABASE_NAME = "wikigame";
+    private final String DB_USER_NAME = "wikigameuser";
+    private final String DB_USER_PASSWD = "asdf1234";
+
+    /**
+     * Liefert eine DB Connection auf die mysql-DB
+     *
+     * @return DB Connection
+     */
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+
+        StringBuilder sbBuilder = new StringBuilder("jdbc:mysql://localhost:3306/");
+        sbBuilder.append(DB_DATABASE_NAME).append("?user=")
+                .append(DB_USER_NAME).append("&password=")
+                .append(DB_USER_PASSWD)
+                .append("&useUnicode=true&characterEncoding=UTF-8");
+
+        conn = DriverManager.getConnection(sbBuilder.toString());
+
+        return conn;
+
+
+    }
+}
