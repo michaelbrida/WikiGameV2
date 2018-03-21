5766ff639cc43d92fcafec6333a4b8df132a7f42
jusic <jusic.husein@hotmail.com>
2018-03-21 15:41:20 +0100

    Login

--- src/at/htld/modules/controller/Launcher.java
+++ src/at/htld/modules/controller/Launcher.java
@@ -1,4 +1,31 @@
 package at.htld.modules.controller;
 
+import at.htld.modules.handler.Session;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+
+
 public class Launcher {
+
+    private Session session;
+
+    @FXML
+    private Text userNameLabel;
+
+
+    @FXML
+    protected void initialize(){
+        userNameLabel.setText("LOL JEBOTE");
+    }
+
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
 }
--- src/at/htld/modules/controller/Register.java
+++ src/at/htld/modules/controller/Register.java
@@ -1,4 +1,9 @@
 package at.htld.modules.controller;
 
+import at.htld.modules.handler.Session;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Register {
+
 }
